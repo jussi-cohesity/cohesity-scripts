94ef23f7ebfa5ab95bf6a0958400794dd396f021
Jussi Jaurola <56082924+jussi-cohesity@users.noreply.github.com>
2019-10-17 09:07:15 +0300

    Update registerSQLServers.ps1

--- registerSQLServers.ps1
+++ registerSQLServers.ps1
@@ -1,34 +1,83 @@
-### usage: ./registerSQLServers.ps1 -vip 192.168.1.198  -serverlist 'servers.txt'
+### usage: ./generateLicenseReport.ps1 -clusterList <file.txt> -username admin [ -domain local ] [-perjobstats true/false] 
 
-### Example script to register hosts and sql service from them - Jussi Jaurola <jussi@cohesity.com>
+### Simple script to generate license consumed report. Requires Cohesity OS 6.4+ - Jussi Jaurola <jussi@cohesity.com>
+
+### clusterlist contains cluster name/ip per line
+
 
 ### process commandline arguments
 [CmdletBinding()]
 param (
-    [Parameter(Mandatory = $True)][string]$vip,
-    [Parameter(Mandatory = $True)][string]$serverlist
+    [Parameter(Mandatory = $True)][string]$clusterList,
+    [Parameter(Mandatory = $True)][string]$username,
+    [Parameter()][string]$domain = 'local',
+    [Parameter()][ValidateSet('true','false')][string]$perjobstats = "false"
 )
-Get-Module -ListAvailable -Name Cohesity* | Import-Module
 
-$servers = Get-Content $serverlist
+### source the cohesity-api helper code
+. ./cohesity-api
 
-# Connect to Cohesity cluster
-try {
-    Connect-CohesityCluster -Server $vip 
-} catch {
-    write-host "Cannot connect to Cohesity cluster $vip" -ForegroundColor Yellow
-    exit
-}
+$clusters = Get-Content $clusterList
 
-# Register physical servers
-$serverCount = $servers.Count
-write-host "Registering $serverCount new sources from $serverlist"
-
-foreach ($server in $servers) 
+foreach ($cluster in $clusters)
 {
-    write-host "Registering host $server as physical host"
-    $a = Register-CohesityProtectionSourcePhysical -HostType KWindows -Server $server -PhysicalType KHost
-    write-host "Registering SQL server from host $server"
-    $serverId = $a.Id
-    $newSQL = Register-CohesityProtectionSourceMSSQL -HasPersistentAgent -Id $serverId
+    ### authenticate
+    apiauth -vip $cluster -username $username -domain $domain
+
+    ### check version
+    $currentCluster = api get cluster
+    $clusterSoftwareVersion = $currentCluster.clusterSoftwareVersion.SubString(0,3)
+
+    if ($clusterSoftwareVersion -lt "6.4") {
+        Write-Host "Cluster software needs to be 6.4 or higher to get metrics!" -ForegroundColor Red
+    }
+
+    ### get platform 
+    $nodes = api get /nodes
+    $hddCapacity = $nodes.capacityByTier | where-object { $_.storageTier -eq 'SATA-HDD'}
+    $platformLicenseTotal = 0
+    foreach ($hdd in $hddCapacity) 
+    {
+        $platformLicenseTotal += $hdd.tierMaxPhysicalCapacityBytes
+    }
+
+    ### get statistics
+    $protectionStats = api get "stats/consumers?allUnderHierarchy=true&consumerType=kProtectionRuns"
+
+    # get view protection jobs
+    $viewProtectionJobs = api get protectionJobs?allUnderHierarchy=true | where-object { $_.environment -eq 'kView' }
+
+    $viewNames = @()
+    foreach ($viewName in $viewProtectionJobs.name)
+    {
+        $viewNames += $viewName
+
+    }
+
+    $statsWithoutViewJobs = $protectionStats.statsList | Where-Object { $_.name -notmatch ('(' + [string]::Join(')|(', $viewNames) + ')') }
+
+    $totalLocalWrittenBytes = 0
+    $totalCloudWrittenBytes= 0
+    $storageConsumedBytes = 0
+
+    if ($perjobstats -eq 'true') {Write-Host "License consumed per job:"}
+
+    foreach ($job in $statsWithoutViewJobs)
+    {
+        $totalLocalWrittenBytes += $job.stats.localDataWrittenBytes
+        $totalCloudWrittenBytes += $job.stats.cloudDataWrittenBytes
+        $storageConsumedBytes += $job.stats.storageConsumedBytes
+        
+        if ($perjobstats -eq 'true') {
+            Write-Host "Protection Job: $($job.name)" -ForegroundColor Yellow
+            Write-Host "DataProtect:  $([math]::Round($job.stats.localDataWrittenBytes/1024/1024/1024)) GiB" -ForegroundColor Yellow
+            Write-Host "CloudArchive: $([math]::Round($job.stats.cloudDataWrittenBytes/1024/1024/1024)) GiB" -ForegroundColor Yellow
+            Write-Host "----------------------------------------------"
+        }
+    }
+
+    Write-Host "Cluster $($currentCluster.name) ($($currentCluster.id)) Total statistics:"
+    Write-Host "DataProtect:  $([math]::Round($totalLocalWrittenBytes/1024/1024/1024)) GiB" 
+    Write-Host "CloudArchive: $([math]::Round($totalCloudWrittenBytes/1024/1024/1024)) GiB" 
+    Write-Host "DataPlatform: $([math]::Round($platformLicenseTotal/1024/1024/1024)) GiB" 
 }
