45ccf6018dbe9809615e023fea101d6b25bc2e63
Jussi Jaurola <56082924+jussi-cohesity@users.noreply.github.com>
2020-09-22 13:44:09 +0300

    Update cohesity-storage-report-helios.ps1
    Added DataProtect license part

--- reporting/cohesity-storage-report-helios.ps1
+++ reporting/cohesity-storage-report-helios.ps1
@@ -3,6 +3,7 @@
 
 ### Note! You need to have cohesity-api.ps1 on same directory!
 
+### Data values are in TIB base2
 
 ### process commandline arguments
 [CmdletBinding()]
@@ -22,6 +23,8 @@ try {
     exit
 }
 
+### Add headers to export-file
+Add-Content -Path $export -Value "Cluster Name, Cluster Storage Consumed, Cluster Protected Objects Count, Cluster Protected Capacity, Cluster DataProtect License Used"
 
 $clusters = $HELIOSCONNECTEDCLUSTERS.name
 foreach ($cluster in $clusters) {
@@ -32,16 +35,20 @@ foreach ($cluster in $clusters) {
     $clusterStats = api get "cluster?fetchTimeSeriesSchema=true&fetchStats=true"
     $clusterEntityId = $clusterStats.schemaInfoList[2].entityId
     $timeSeriesStats =  api get "statistics/timeSeriesStats?startTimeMsecs=1600214598804&schemaName=kBridgeClusterTierPhysicalStats&metricName=kMorphedUsageBytes&rollupIntervalSecs=21600&rollupFunction=latest&entityIdList=$clusterEntityId"
-    $clusterStorageConsumedBytes = $timeSeriesStats.dataPointVec[0].data.int64Value
+    $clusterStorageConsumedBytes = [math]::Round(($timeSeriesStats.dataPointVec[0].data.int64Value/1TB),1)
 
     ### Get clusterProtectedObjects and clusterProtectedObjectCapacity
     $protectionSources =api get "protectionSources/registrationInfo?includeEntityPermissionInfo=true"
     $clusterProtectedObjects = $protectionSources.stats.protectedCount
-    $clusterProtectedCapacity = $protectionSources.stats.protectedSize
-
+    $clusterProtectedCapacity = [math]::Round(($protectionSources.stats.protectedSize/1TB),1)
+    
+    ### Get DataProtection usage
+    $storageStats = api get "mcm/stats/storage"
+    $clusterDataProtectUsed = [math]::Round(($storageStats.dataProtectionPhysicalUsageBytes/1TB),1)
+   
     if ($export) {
         ## write data 
-        $line = "{0},{1},{2},{3}" -f $cluster, $clusterStorageConsumedBytes, $clusterProtectedObjects, $clusterProtectedCapacity
+        $line = "{0},{1},{2},{3},{4}" -f $cluster, $clusterStorageConsumedBytes, $clusterProtectedObjects, $clusterProtectedCapacity, $clusterDataProtectUsed
         Add-Content -Path $export -Value $line
 
     }
