c755122bf7f2e88650ff01e77023b4f4d92a88fd
Jussi Jaurola <56082924+jussi-cohesity@users.noreply.github.com>
2020-11-30 13:53:41 +0200

    Create cohesity-reporting-agent-runs.ps1

---
+++ reporting/cohesity-reporting-agent-runs.ps1
@@ -0,0 +1,105 @@
+### usage: ./cohesity-reporting-agent-runs.ps1 -vip 192.168.1.198 -username admin [ -domain local ] [-export 'filename.csv']
+
+### Sample script to report physical agent front end capacity - Jussi Jaurola <jussi@cohesity.com>
+
+
+### process commandline arguments
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $True)][string]$vip, 
+    [Parameter(Mandatory = $True)][string]$username,
+    [Parameter()][string]$domain = 'local',
+    [Parameter()][string]$export 
+    )
+
+### source the cohesity-api helper code 
+. ./cohesity-api.ps1
+
+try {
+    apiauth -vip $vip -username $username -domain $domain
+    $clusterName = (api get cluster).name
+} catch {
+    write-host "Cannot connect to cluster $vip" -ForegroundColor Yellow
+    exit
+}
+
+if ($export) {
+    ### Add headers to export-file
+    Add-Content -Path $export -Value "Organsation Id, Protection Group, Source, Data Read (bytes)"
+}
+
+$report = @{}
+
+### Get mSec time for days 
+$date = [DateTime]::Today.AddHours(23).AddMinutes(59).AddSeconds(59)
+$endTimeMsecs = [DateTimeOffset]::new($date).ToUnixTimeMilliSeconds()
+
+Write-Host "Collecting stats for non-Tenant sources"
+$jobs = api get "data-protect/protection-groups?isDeleted=false&includeTenants=false&includeLastRunInfo=true&environments=kPhysical" -v2
+foreach ($job in $jobs.protectionGroups) {
+    $jobName = $job.name
+    $jobId = $job.id.split(':')[2]
+    Write-Host "        Collecting stats for Protection Group $($job.name)"
+    $runs = api get protectionRuns?jobId=$($jobId)`&excludeNonRestoreableRuns=true
+    foreach ($run in $runs) {
+        if ($run.backupRun.snapshotsDeleted -eq $false) {
+            foreach($source in $run.backupRun.sourceBackupStatus) {
+                $sourcename = $source.source.name
+                if($sourcename -notin $report.Keys) {
+                    $report[$sourcename] = @{}
+                    $report[$sourcename]['organisationId'] = "SP"
+                    $report[$sourcename]['protectionGroup'] = $jobName
+                    $report[$sourcename]['size'] = 0
+                }
+                $report[$sourcename]['size'] += $source.stats.totalBytesReadFromSource
+            }                    
+        }
+    }
+}
+
+
+Write-Host "Collecting stats for tenants"
+$tenants = api get tenants
+
+foreach ($tenant in $tenants) {
+    $tenantName = $tenant.name
+    $tenantId = $tenant.tenantId
+    $tenantId = $tenantId.Substring(0,$tenantId.Length-1)
+
+    Write-Host "    Collecting $tenantName stats"
+    
+    apiauth -vip $vip -username $username -tenantId $tenantId
+
+    $jobs = api get "data-protect/protection-groups?isDeleted=false&includeTenants=true&includeLastRunInfo=true&environments=kPhysical" -v2
+    foreach ($job in $jobs.protectionGroups) {
+        $jobName = $job.name
+        $jobId = $job.id.split(':')[2]
+        Write-Host "        Collecting stats for Protection Group $($job.name)"
+            $runs = api get protectionRuns?jobId=$($jobId)`&excludeNonRestoreableRuns=true
+            foreach ($run in $runs) {
+                if ($run.backupRun.snapshotsDeleted -eq $false) {
+                    foreach($source in $run.backupRun.sourceBackupStatus) {
+                        $sourcename = $source.source.name
+                        if($sourcename -notin $report.Keys){
+                            $report[$sourcename] = @{}
+                            $report[$sourcename]['organisationId'] = $tenantId
+                            $report[$sourcename]['protectionGroup'] = $jobName
+                            $report[$sourcename]['size'] = 0
+                            
+                        }
+                        $report[$sourcename]['size'] += $source.stats.totalBytesReadFromSource
+                    }
+                }
+            }
+        
+    }
+}
+
+### Export data
+if ($export) {
+    $report.GetEnumerator() | Sort-Object -Property {$_.Value.tenant} | ForEach-Object {
+        $line = "{0},{1},{2},{3}" -f $_.Value.organisationId, $_.Value.protectionGroup, $_.Name, $_.Value.size
+        Add-Content -Path $export -Value $line
+    }
+
+}
