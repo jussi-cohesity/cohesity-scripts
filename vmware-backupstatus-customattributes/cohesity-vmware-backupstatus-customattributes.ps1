7179002e933c5c7446b40cc10ffa7106f37c7615
Jussi Jaurola <56082924+jussi-cohesity@users.noreply.github.com>
2021-03-18 16:53:04 +0200

    Create cohesity-vmware-backupstatus-customattributes.ps1

---
+++ vmware-backupstatus-customattributes/cohesity-vmware-backupstatus-customattributes.ps1
@@ -0,0 +1,71 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $True)][string]$cohesityCluster, #the cluster to connect to (DNS name or IP)
+    [Parameter(Mandatory = $True)][string]$cohesityCred, #credentials file for cohesity
+    [Parameter(Mandatory = $True)][string]$vmwareCred #credentials file for cohesity
+)
+
+Write-Host "Importing credentials from credential file $($cohesityCred)" -ForegroundColor Yellow
+Write-Host "Connecting to Cohesity Cluster $($cohesityCluster)" -ForegroundColor Yellow
+
+$Credential = Import-Clixml -Path ($cohesityCred)
+try {
+    Connect-CohesityCluster -Server $cohesityCluster -Credential $Credential
+    Write-Host "Connected to Cohesity Cluster $($cohesityCluster)" -ForegroundColor Yellow
+} catch {
+    write-host "Cannot connect to Cohesity cluster $($cohesityCluster)" -ForegroundColor Yellow
+    exit
+}
+
+### Search VMware objects protected
+
+Write-Host "Getting VMware Object protection statusinfo"
+
+$report = @{}
+
+$protectionSourceObjects = Get-CohesityProtectionSourceObject KVMware
+$sources = $protectionSourceObjects | Where-Object { $_.vmWareProtectionSource.type -eq 'kVirtualMachine' }
+
+foreach ($source in $sources) {
+    $lastrun = Get-CohesityProtectionJobrun -Sourceid ($source.id) -numruns 1
+    if ($lastrun) {
+        $sourceName = $source.name
+        Write-Host "Getting details for $sourceName"
+
+        $lastRunStatus = $lastrun.backupRun.status
+        $lastRunStartUsecs = Convert-CohesityUsecsToDateTime -usecs ($lastrun.backupRun.stats.startTimeUsecs)
+        $lastRunTimeStamp = $lastRunStartUsecs.dateTime
+        $sourceParent = $protectionSourceObjects | Where-Object { $_.id -eq $source.parentId }
+
+        
+        if($sourceName -notin $report.Keus) {
+            $report[$sourcename] = @{}
+            $report[$sourcename]['lastRunStatus'] = $lastRunStatus
+            $report[$sourcename]['lastRunTimeStamp'] = $lastRunTimeStamp
+            $report[$sourcename]['vCenter'] = $sourceParent.name
+        }
+    }
+}
+
+## Change attributes
+
+$report.GetEnumerator() | Sort-Object -Property {$_.Value.vCenter} | ForEach-Object {
+    
+    if ($_.Value.vCenter -eq $connectedVcenter) {
+        df
+    } else {
+        ### Connect to VMware vCenter
+        $vmwareCredential = Import-Clixml -Path ($vmwareCred)
+        try {
+            Connect-VIServer -Server $vmwareEnvironment -Credential $vmwareCredential
+            Write-Host "Connected to VMware vCenter $($global:DefaultVIServer.Name)" -ForegroundColor Yellow
+            $connectedVcenter = $($global:DefaultVIServer.Name)
+        } catch {
+            write-host "Cannot connect to VMware vCenter $($vmwareEnvironment)" -ForegroundColor Yellow
+            exit
+        }
+    }
+    
+
+
+}
