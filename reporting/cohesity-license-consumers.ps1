4881f0606bf746af0d3dfcbd137ba1e05d27b075
Jussi Jaurola <56082924+jussi-cohesity@users.noreply.github.com>
2021-07-28 15:07:19 +0300

    Update cohesity-license-consumers.ps1

--- reporting/cohesity-license-consumers.ps1
+++ reporting/cohesity-license-consumers.ps1
@@ -1,44 +1,51 @@
-### Example script to get cluster stats from local clusters - Jussi Jaurola <jussi@cohesity.com
+### Example script to get cluster stats from helios - Jussi Jaurola <jussi@cohesity.com>
 
 ### Note! You need to have cohesity-api.ps1 on same directory!
 
+### Data values are in TIB base2
 
 ### process commandline arguments
 [CmdletBinding()]
 param (
-    [Parameter(Mandatory = $True)][string]$vip, 
-    [Parameter(Mandatory = $True)][string]$username,
+    [Parameter(Mandatory = $True)][string]$apikey,
     [Parameter()][ValidateSet('MB','GB','TB')][string]$unit = "MB",
     [Parameter(Mandatory = $true)][string]$export 
     )
+
 
 ### source the cohesity-api helper code 
 . ./cohesity-api.ps1
 
 try {
-    apiauth -vip $vip -username $username 
-    $clusterName = (api get cluster).name
+    apiauth -helios -password $apikey
 } catch {
-    write-host "Cannot connect to cluster $vip" -ForegroundColor Yellow
+    write-host "Cannot connect to Helios. Please check the apikey!" -ForegroundColor Yellow
     exit
 }
 
 ### Add headers to export-file
-Add-Content -Path $export -Value "Cluster Name, Tenant Name, Protection Group, Environment Type, Storage Consumed ($unit)"
+Add-Content -Path $export -Value "Cluster Name, Tenant Name, Protection Group, Environment Type, Local Storage Consumed ($unit), Archive Storage Consumed ($unit)"
 
-### Get usage stats
 $units = "1" + $unit
-$stats = api get "stats/consumers?maxCount=10000&fetchViewBoxName=true&fetchTenantName=true&fetchProtectionEnvironment=true&consumerType=kProtectionRuns"
 
-foreach ($stat in $stats.statsList) {
+$clusters = $HELIOSCONNECTEDCLUSTERS.name
+foreach ($cluster in $clusters) {
+    ## Conenct to cluster
+    heliosCluster $cluster
+    
+    $stats = api get "stats/consumers?maxCount=10000&fetchViewBoxName=true&fetchTenantName=true&fetchProtectionEnvironment=true&consumerType=kProtectionRuns"
 
-    $jobname = $stat.name
-    $tenantName = $stat.groupList.tenantName
-    $environment = $stat.protectionEnvironment
+    foreach ($stat in $stats.statsList) {
 
-    $storageConsumed = $stat.stats.storageConsumedBytes/$units
+        $jobname = $stat.name
+        $tenantName = $stat.groupList.tenantName
+        $environment = $stat.protectionEnvironment
 
-    ### write data 
-    $line = "{0},{1},{2},{3},{4}" -f $clusterName, $tenantName, $jobName, $environment, $storageConsumed
-    Add-Content -Path $export -Value $line
+        $localStorageConsumed = $stat.stats.storageConsumedBytes/$units
+        $archiveStorageConsumed = $stat.stats.localDataWrittenBytes/$units
+
+        ### write data 
+        $line = "{0},{1},{2},{3},{4}" -f $clusterName, $tenantName, $jobName, $environment, $localStorageConsumed, $archiveStorageConsumed
+        Add-Content -Path $export -Value $line
+    }
 }
