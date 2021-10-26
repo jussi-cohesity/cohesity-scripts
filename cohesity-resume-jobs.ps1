b540671807c0a2b62ad70abcbb217c16b7a1c0b1
Jussi Jaurola <56082924+jussi-cohesity@users.noreply.github.com>
2021-10-26 10:30:05 +0300

    Create cohesity-resume-jobs.ps1

---
+++ cohesity-resume-jobs.ps1
@@ -0,0 +1,24 @@
+### Sample script to resume jobs - Jussi Jaurola <jussi@cohesity.com>
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $True)][string]$username,
+    [Parameter(Mandatory = $true)][string]$password,
+    [Parameter(Mandatory = $true)][string]$cohesityCluster
+    )
+
+Get-Module -ListAvailable -Name Cohesity* | Import-Module
+
+try {
+    Connect-CohesityCluster -Server $cohesityCluster -Credential (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, (ConvertTo-SecureString -AsPlainText $password -Force))
+} catch {
+    Write-Error "Cannot connect to Cohesity cluster $($cohesityCluster)"
+}
+
+### Define jobs to be suspended
+$jobArray = "JOBNAME_12months", "JOBNAME_6monts", "JOBNAME_1month"
+
+foreach ($job in $jobArray) {
+    Write-Host "Resuming job $job"
+    Resume-CohesityProtectionJob -Name $job
+}
