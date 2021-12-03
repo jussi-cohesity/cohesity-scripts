50e066226b8df82fdc224305843472833b7ac7e4
Jussi Jaurola <56082924+jussi-cohesity@users.noreply.github.com>
2021-12-03 08:42:11 +0200

    Create cohesity-dmaas-m365-sizing-preregs.ps1

---
+++ M365/cohesity-dmaas-m365-sizing-preregs.ps1
@@ -0,0 +1,19 @@
+### This script will install needed modules to use M365 Sizer tool - Jussi Jaurola <jussi@cohesity.com>
+
+
+$modules = @("Microsoft.Graph.Reports", "ExchangeOnlineManagement","PSWSMan")
+
+foreach($module in $modules)
+{
+    Write-Host "Checking module $module" -ForegroundColor Yellow
+    if (!(Get-Module -ListAvailable -Name $module)) {
+      Write-Host "    Installing module $module" -ForegroundColor Yellow
+      Install-Module $module -Scope CurrentUser -Force
+      if ($module -eq 'PSWSMan') {
+        Write-Host "Module PWSMan requires additional command also. Running Install-WSMan" -ForegroundColor Yellow
+        Install-WSMan
+      }
+    } else {
+        Write-Host "   Module found!" -ForegroundColor Yellow
+    }
+}
