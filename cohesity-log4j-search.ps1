1e8912202ac1316992d0e1f8515fb7c9e782b296
Jussi Jaurola <56082924+jussi-cohesity@users.noreply.github.com>
2021-12-20 19:32:47 +0200

    Update cohesity-log4j-search.ps1

--- cohesity-log4j-search.ps1
+++ cohesity-log4j-search.ps1
@@ -24,5 +24,4 @@ try {
     Write-Error "Cannot connect to Cohesity cluster $($cohesityCluster)"
 }
 
-Find-CohesityFilesForRestore -Search log4j-core | Select-Object Filename,@{Name="Name"; Expression={$_.ProtectionSource.name}}
-
+Find-CohesityFilesForRestore -Search log4j-core | Select-Object Filename,@{Name="Source"; Expression={$_.ProtectionSource.name}}
