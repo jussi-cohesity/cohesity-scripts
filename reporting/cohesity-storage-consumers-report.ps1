a1f9a93f41bbc806feff487ecbbc5b9f9d285f47
Jussi Jaurola <56082924+jussi-cohesity@users.noreply.github.com>
2022-09-27 09:59:22 +0300

    Update cohesity-storage-consumers-report.ps1

--- reporting/cohesity-storage-consumers-report.ps1
+++ reporting/cohesity-storage-consumers-report.ps1
@@ -24,17 +24,19 @@ try {
 }
 
 ### Add headers to export-file
-Add-Content -Path $export -Value "Cluster Name, Tenant Name, Protection Group, Environment Type, Data In ($unit), Data In After Dedupe ($unit), Local Data Written ($unit), Cloud Data Written ($unit), Storage Consumed ($unit)"
+Add-Content -Path $export -Value "Cluster Name, Tenant Name, Protection Group, Environment Type, Source Count, Data In ($unit), Data In After Dedupe ($unit), Local Data Written ($unit), Cloud Data Written ($unit), Storage Consumed ($unit)"
 
 ### Get usage stats
 $units = "1" + $unit
 $stats = api get "stats/consumers?maxCount=1000&fetchViewBoxName=true&fetchTenantName=true&fetchProtectionEnvironment=true&consumerType=kProtectionRuns"
-
+$jobs = api get protectionJobs
 foreach ($stat in $stats.statsList) {
 
     $jobname = $stat.name
     $tenantName = $stat.groupList.tenantName
     $environment = $stat.protectionEnvironment
+    
+    $sourceCount = ($jobs | Where { $_.name -eq $jobname }).sourceIds.count
 
     $dataIn = $stat.stats.dataInBytes/$units
     $dataInDeduped = $stat.stats.dataInBytesAfterDedup/$units
@@ -43,6 +45,6 @@ foreach ($stat in $stats.statsList) {
     $storageConsumed = $stat.stats.storageConsumedBytes/$units
 
     ### write data 
-    $line = "{0},{1},{2},{3},{4},{5},{6},{7},{8}" -f $clusterName, $tenantName, $jobName, $environment, $dataIn, $dataInDeduped, $localDataWritten, $cloudDataWritten, $storageConsumed
+    $line = "{0},{1},{2},{3},{4},{5},{6},{7},{8},{9}" -f $clusterName, $tenantName, $jobName, $environment, $sourceCount, $dataIn, $dataInDeduped, $localDataWritten, $cloudDataWritten, $storageConsumed
     Add-Content -Path $export -Value $line
 }
